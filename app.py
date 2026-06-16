a5e49d7dd07dae6a5510495cc87d65a65dff3d9d
DS.Software <talysonprogamadorpython@gmail.com>
2026-06-16 19:29:57 -0300

    adiciona funcionalidade para processar arquivos CSV e calcular somas

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+
+st. title("Resultado de somas repetidas.")
+
+numero = st.number_input("Quantos aquivos deseja processar", min_value=1, step=1)
+
+for i in range(numero):
+    i = i+1
+    st.write(f"Arquivo {i}")
+    arquivo = st.file_uploader(f"Escolha o arquivo ", type=["csv"])
+    if arquivo is not None:
+        df = pd.read_csv(arquivo)
+        st.write(df)
+        resultado = df.sum().sum()
+        st.write(f"O resultado da soma do arquivo {i} é: {resultado}")
