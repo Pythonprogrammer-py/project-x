cdfa63e4c64b8e4a3ccad1926c5e2fddecef93fb
DS.Software <talysonprogamadorpython@gmail.com>
2026-06-26 23:43:15 -0300

    menu estásendo reformado

--- Main.py
+++ Main.py
@@ -4,4 +4,4 @@ import pandas as pd
 
 
 # 1. Criação do menu (pode ser na barra lateral ou no topo)
-home, servicos, contato , descricao = st.tabs(home="Home", servicos="Serviços", contato="Contato", descricao="Descrição")
+st.tabs(home="Home", servicos="Serviços", contato="Contato", descricao="Descrição")
